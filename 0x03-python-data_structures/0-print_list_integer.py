2e29667f4aa64f1eda4b9ab52cf44b06fb31da41
Fatthallah <eng.fatthallah@gmail.com>
2023-10-09 14:48:21 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
+
 
 def print_list_integer(my_list=[]):
+    """Comment"""
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
 
