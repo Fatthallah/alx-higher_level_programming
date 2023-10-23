9b039d361e00d9c5198fc275a3f0f1789565eba4
Fatthallah <eng.fatthallah@gmail.com>
2023-10-23 13:56:14 -0700

    x

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    final = 0
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception('Too far')
+            else:
+                final = final + (a ** b / x)
+        except Exception:
+            final = b + a
+            break
+    return (final)
+
