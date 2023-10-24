75404d2e202de4a0fb267d77477b17229689130a
Fatthallah <eng.fatthallah@gmail.com>
2023-10-24 14:15:48 -0700

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Comment 1"""
+
+class Square: pass
+"""Comment 2"""
+
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""SHN"""
+
+class Square:
+    """SHN"""
+
+    def __init__(self, size):
+        """SHN"""
+        self.__size = size
+
