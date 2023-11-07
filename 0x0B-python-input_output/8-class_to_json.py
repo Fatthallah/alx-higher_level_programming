9ba2589fd716593be4a0101a2cf6aeb2e953e291
Fatthallah <eng.fatthallah@gmail.com>
2023-11-07 08:30:59 -0800

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""u"""
+def class_to_json(obj):
+    """qw"""
+    return obj.__dict__
+
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""j"""
+class Student:
+    """q"""
+    def __init__(self, first_name, last_name, age):
+        """jk"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """k"""
+        return self.__dict__
+
