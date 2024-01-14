cb419d6e5494e6132ef844d4bc7081c16458d15e
Fatthallah <eng.fatthallah@gmail.com>
2024-01-14 13:50:08 -0800

    x

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-The Comment 
-"""
+""" The Comment """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
-
 class State(Base):
-    """
-    The Comment
-    """
+    """ The Comment """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
