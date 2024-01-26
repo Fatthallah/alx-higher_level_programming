29b36a61c95692e67baa6c932566245f4c0a8a86
Fatthallah <eng.fatthallah@gmail.com>
2024-01-26 11:57:11 -0800

    x

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+The Comment
+"""
+
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        value = html.get('X-Request-Id')
+        print(value)
