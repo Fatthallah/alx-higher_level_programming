62016a27285a5cadc50d0df6504f9d3629dd79fa
Fatthallah <eng.fatthallah@gmail.com>
2024-01-26 12:04:57 -0800

    x

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+The Comment
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    body = response.text
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(body)
